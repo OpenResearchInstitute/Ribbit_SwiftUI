397f8689275d429763b594a84005feab603a12bd
Ahmet Inan <xdsopl@gmail.com>
2023-03-06 13:19:16 +0100

    Initial commit

---
+++ Ribbit/RibbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RibbitApp.swift
+//  Ribbit
+//
+//  Created by Ahmet Inan on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RibbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
